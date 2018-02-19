fe0650b567e46be89f612645a1d0047767fe7d3f
rammaddela <ram.k.maddela@gmail.com>
2018-02-18 20:29:25 -0800

    pushing the local changes

---
+++ target/test-classes/sqlscript.sql
@@ -0,0 +1,25 @@
+-- -- Table: public.trackrecord
+
+-- -- DROP TABLE public.trackrecord;
+
+-- CREATE TABLE public.trackrecord
+-- (
+--     cardholder_name character varying(20) COLLATE pg_catalog."default",
+--     control_no character varying(20) COLLATE pg_catalog."default",
+--     expiry_date date,
+--     id bigint NOT NULL,
+--     lot_no character varying(20) COLLATE pg_catalog."default",
+--     "CARD_DESIGN_ID" character varying(20) COLLATE pg_catalog."default",
+--     card_design_id character varying(255) COLLATE pg_catalog."default",
+--     ship_name character varying(255) COLLATE pg_catalog."default",
+--     create_ts timestamp without time zone DEFAULT now(),
+--     card_no character varying(255) COLLATE pg_catalog."default",
+--     CONSTRAINT trackrecord_pkey PRIMARY KEY (id)
+-- )
+-- WITH (
+--     OIDS = FALSE
+-- )
+-- TABLESPACE pg_default;
+
+-- ALTER TABLE public.trackrecord
+--     OWNER to postgres;
